1af1c0ac3c3be8df8d0acca7a136f31224cb7605
USER_GYM <maribelfernandezlopez0@gmail.com>
2026-01-05 12:50:54 -0600

    Add files via upload

--- sw.js.js
+++ sw.js.js
@@ -1,7 +1,17 @@
-const CACHE = "smg-nexus-v3";
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html"])));
+const CACHE_NAME = 'titan-os-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Rajdhani:wght@500;700&family=Roboto+Mono:wght@400;700&family=Inter:wght@300;400;800&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
